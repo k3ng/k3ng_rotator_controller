729559999d16e35097aaf14bedb8a177ef711e11
barisdinc <barisdinc@gmail.com>
2017-12-02 22:19:48 +0300

    GLCD with openGLCD added...

---
+++ k3ng_rotator_controller/rotator_pins_glcd.h
@@ -0,0 +1,50 @@
+/*
+ * GLCD pins for Graphic LCD support by TA7W Baris
+ * 
+ * Tested on powertip GLCD pg12864d but can be extended to other GLCDs that are
+ * supported by openlcd library
+ * 
+ */
+
+ 
+
+/*
+ * all definitions are in the file located at libraries/openGLCD/config/ks0108  or somewhere in config directory
+ * Please use the related pins for your choice of Arduino board
+ * 
+ * I placed config for Arduino MEGA for reference, but please check the config directory for mode  information
+ */
+
+
+//Graphic LCD support by TA7W
+// #include <openGLCD.h>
+// #include <include/openGLCD_GLCDv3.h> // GLCDv3 compatibilty mode
+// #include "fonts/allFonts.h"         // system and arial14 fonts are used
+// #include "bitmaps/allBitmaps.h"       // all images in the bitmap dir 
+
+/*
+ // Data pin definitions
+// This version uses pins 22-29 for LCD Data 
+ 
+#define glcdPinData0    22
+#define glcdPinData1    23
+#define glcdPinData2    24
+#define glcdPinData3    25
+#define glcdPinData4    26
+#define glcdPinData5    27
+#define glcdPinData6    28
+#define glcdPinData7    29
+
+
+// Pins used for Control
+ 
+#define glcdPinCSEL1     33    // CS1 Bit  
+#define glcdPinCSEL2     34    // CS2 Bit
+#define glcdPinRW        35    // R/W Bit
+#define glcdPinDI        36    // D/I Bit 
+#define glcdPinEN        37    // EN Bit
+
+*/
+
+
+
